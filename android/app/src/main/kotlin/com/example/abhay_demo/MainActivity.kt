0a2b6e2321a41aa5159550b07f6a9fcdb4e6fef6
ashishk147 <iamashishkumar147@gmail.com>
2024-10-17 16:22:02 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/abhay_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.abhay_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/login.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Mylogin extends StatelessWidget {
+  const Mylogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Loginpage"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Text("WElcome"),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
